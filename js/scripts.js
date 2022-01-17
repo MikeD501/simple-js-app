e14dc61877eb48d922667cc29f99a001f72c9269
MikeD501 <mike.dziewit@yahoo.com>
2022-01-17 13:31:48 -0600

    first draft
    first draft

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,29 @@
-alert('Hello World');
+let pokemon = 'Charizard';
+let age = 3;
+let weight = 300;
 
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let Charizard = [
+    'Charizard ',
+    'age 3yo ',
+    'height 7ft ',
+]
+
+let Venusaur = [
+    'Venusaur ',
+    'age of 4yo ',
+    'height 8ft ',
+]
+
+let Blastoise = [
+    'Blastoise',
+    'age 5yo ',
+    'height of 12ft ',
+]
+
+let pokemonlist = [
+    Charizard,
+    Venusaur,
+    Blastoise,
+]
+
+document.write(pokemonlist);
