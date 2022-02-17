49c187122468c24f11bb5bdc44430bc255c671b7
MikeD501 <mike.dziewit@yahoo.com>
2022-02-17 01:27:33 -0600

    Update scripts.js
    for loop and if statement added for practice

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-const pokemonList = [
+let pokemonList = [
     {
         name: 'Charizard',
         age: 3,
@@ -21,4 +21,15 @@ const pokemonList = [
     }
 ];
 
-document.write(JSON.stringify(pokemonList));
+ // document.write(JSON.stringify(pokemonList));  //
+
+for (let i=0; i < pokemonList.length; i++) /*this loop starts at 0, counts items in array, and adds 1 each time it runs*/
+ {
+    document.write(pokemonList[i].name + " is " + pokemonList[i].height + "ft tall. " + "<br/>");
+    /* The above command writes the name of the pokemon with its height in the browser */
+
+        if (pokemonList[i].height > 10) {
+            document.write( " -- Wow, thats big! ");
+        }
+        /*the above if statement checks if a pokemon is above 10 tall and if it is it displays a special msg */
+}
